cf45df913429dddbd393ad1d045cbd6676677a4c
dima <dimazigulin2213@gmail.com>
2026-01-04 21:14:56 +0300

    new feature: created the first element: external. started

---
+++ external_data.py
@@ -0,0 +1,25 @@
+from init_logger import logger
+
+class External:
+    def __init__(self, url: str, params: dict, api_key: str):
+        if url:
+            self.url = url
+            logger.info("added url data: %s", self.url)
+        else:
+            logger.debug("the transmitted data was empty")
+
+        if api_key:
+            self.api_key = api_key
+            logger.info("added api_key data: %s", self.api_key)
+        else:
+            logger.debug("the transmitted data was empty")
+
+        if params:
+            self.params = params
+            logger.info("added list params: %s", self.params)
+        else:
+            logger.debug("the transmitted data was empty")
+
+if __name__ == "__main__":
+    ex = External("a", {"a": 1}, "demo")
+    logger.info("the object has been created")
