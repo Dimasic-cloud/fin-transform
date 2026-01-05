88cf58ce96055f3211060c6a51d56da89cbf396a
dima <dimazigulin2213@gmail.com>
2026-01-05 21:26:53 +0300

    fixing logic in initialization

--- etl_process/external.py
+++ etl_process/external.py
@@ -1,27 +1,40 @@
 # class for exports data and wrighting it to file
 
+import sys
+from pathlib import Path
+
+# setting the parent directory
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from core.logger import logger
 
+
 class External:
-    # initialization with simple checking input data
     def __init__(self, url: str, params: dict, api_key: str):
-        if url:
-            self.url = url
-            logger.info("added url data: %s", self.url)
-        else:
-            logger.debug("the transmitted data was empty")
+        """
+        initialization with simple checking input data
+        
+        :param self: link to object
+        :param url: URL adress to data extraction website
+        :param params: dictionary with query parameters
+        :param api_key: API key for access data on website
+        """
+        self._url = self._checking(url)
+        self._api_key = self._checking(api_key)
+        self._params = self._checking(params)
 
-        if api_key:
-            self.api_key = api_key
-            logger.info("added api_key data: %s", self.api_key)
-        else:
-            logger.debug("the transmitted data was empty")
+    def _checking(self, elem):
+        """
+            checking input data
 
-        if params:
-            self.params = params
-            logger.info("added list params: %s", self.params)
-        else:
-            logger.debug("the transmitted data was empty")
+            :param elem: param for validation
+        """
+        if elem:
+            logger.debug(f"success! {elem} has been correct")
+            return elem
+
+        logger.error(f"incorrect data: {elem}")
+
 
 if __name__ == "__main__":
     ex = External("a", {"a": 1}, "demo")
